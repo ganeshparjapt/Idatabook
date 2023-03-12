273b41198e8fde34b59e03bdfc5dd373428e4364
ganesh <ganeshparjaptm5@gmail.com>
2023-03-12 16:41:24 +0530

    add some requst response v-44

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//Avable routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
 app.get('/', (req, res) => {
   res.send('Hello World! i am gaensh in learing stage')
 })
 
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+  console.log(` listening on port http://localhost:${port}`)
 })
---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+
+const UserSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: email,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    date:{
+        type: Date,
+        
+    }
+ 
+});
+module.exports = mongoose.model('user',UserSchema)
